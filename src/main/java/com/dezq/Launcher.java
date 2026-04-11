2222e420a99c2543694e9edbedf4c697508b03f5
bishben <bishben10@gmail.com>
2026-04-11 20:50:10 +0530

    fresh start

--- src/main/java/com/dezq/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.dezq;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // This redirects to your Main class but tricks the JVM 
-        // into ignoring the missing JavaFX module-path check.
-        Main.main(args);
-    }
-}
