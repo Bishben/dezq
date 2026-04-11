015e91a98376fb1b621fa213c542c18a43344923
bishben <bishben10@gmail.com>
2026-04-11 23:44:58 +0530

    added basic proxy and tested

--- proxy/Main.java
+++ proxy/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proxy starting...");
+        int port = 8888;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress());
+
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
